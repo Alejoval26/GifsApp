ea852438caf20a23a39f7f563cc60ad0594d168f
Alejoval26 <ing.alejoval26@gmail.com>
2024-02-27 14:03:17 -0500

    creacioón del viwe child en el buscador

--- src/app/gifs/components/searchbox/searchbox.component.ts
+++ src/app/gifs/components/searchbox/searchbox.component.ts
@@ -1,15 +1,25 @@
-import { Component} from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'gifs-searchbox',
   template:`
     <h5>Buscar</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs...">
+    <input type="text"
+    class="form-control"
+    placeholder="Buscar gifs..."
+    (keyup.enter)="searchTag()"#txTTagInput>
   `
 })
 
 export class SearchboxComponent  {
+
+  @ViewChild('txTTagInput')
+  public tagInpuT!:ElementRef<HTMLInputElement>;
+
   constructor() { }
 
-
+  searchTag():void{
+    const newTag = this.tagInpuT.nativeElement.value;
+    console.log(newTag);
+  }
 }
